d2e66a87256e9f28a65d922ec30d455c3fd22f43
Ciprian Nicuta <ciprian.nicuta@endava.com>
2018-09-28 11:49:16 +0300

    method improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("feature/xFeature");
+        int a = 1;
+        while(a<=5){
+            System.out.println(a);
+            a++;
+        }
     }
 }
