206846d5095665af6c231e5136d73d6ddc4425cc
Ciprian Nicuta <ciprian.nicuta@endava.com>
2018-09-28 11:54:10 +0300

    text improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public void init(){
-        System.out.println("sout from yFeature init");
+        System.out.println("sout from yFeature init_ialization");
         System.out.println("one more row");
+
     }
 }
