bfcd65d319dc2dacf10bc7a7a21c4870393beab1
Ciprian Nicuta <ciprian.nicuta@endava.com>
2018-09-28 12:00:27 +0300

    first message improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public void zFeature(){
-        System.out.println("z feature added");
+        System.out.println("z feature added and changed ");
 
         System.out.println("MORE POWER ! ! !");
+
+
     }
 }
